fc7afb4aabbde17232a721659a66204b2f44a8de
VoQn <voqn.tyrantist@gmail.com>
2014-09-06 18:13:11 +0900

    Added Parser test

---
+++ test/Chapter4/ParserSpec.hs
@@ -0,0 +1,43 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Chapter4.ParserSpec where
+
+import Test.Hspec
+import Text.Parsec.Error
+
+import Chapter4.Syntax
+import Chapter4.Parser
+
+instance Eq ParseError where
+   a == b = errorMessages a == errorMessages b
+
+spec :: Spec
+spec = do
+  describe "token parser" $ do
+
+    it "can parse \"true\" -> true" $
+      parseExpr "true" `shouldBe` Right TmTrue
+
+    it "can parse \"false\" -> false" $
+      parseExpr "false" `shouldBe` Right TmFalse
+
+    it "can parse \"0\" -> 0" $
+      parseExpr "0" `shouldBe` Right TmZero
+
+    it "can parse \"(if true false true)\" -> (if true false true)" $
+      parseExpr "(if true false true)" `shouldBe`
+      Right (TmIf TmTrue TmFalse TmTrue)
+
+    it "can parse \"(succ 0)\" -> (succ 0)" $
+      parseExpr "(succ 0)" `shouldBe`
+      Right (TmSucc TmZero)
+
+    it "can parse \"(pred 0)\" -> (pred 0)" $
+      parseExpr "(pred 0)" `shouldBe`
+      Right (TmPred TmZero)
+
+    it "can parse \'(zero? 0)\" -> (zero? 0)" $
+      parseExpr "(zero? 0)" `shouldBe`
+      Right (TmIsZero TmZero)
+
+    it "can parse \"(true)\" -> true" $
+      parseExpr "(true)" `shouldBe` Right TmTrue
