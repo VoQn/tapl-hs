ab51d2cc1efdce263b99ac2048a1761dad769976
VoQn <voqn.tyrantist@gmail.com>
2014-09-17 00:13:42 +0900

    Added Chapter7 REPL

---
+++ src/Chapter7.hs
@@ -0,0 +1,19 @@
+module Chapter7
+  ( module Data.Display
+  , module Chapter7.Syntax
+  , module Chapter7.Parser
+  , module Chapter7.Eval
+  , process
+  ) where
+
+import Data.Display
+import Chapter7.Syntax
+import Chapter7.Parser (parseExpr)
+import Chapter7.Eval
+
+process :: String -> IO ()
+process line = case parseExpr line of
+  Left  err  -> print err
+  Right expr -> case eval [] expr of
+    Left  err  -> display err
+    Right term -> display (withContext [] term)
--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,46 @@
-{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import Chapter4 as Ch4
+import Chapter7 as Ch7
 
 import Control.Monad.Trans
 import System.Console.Haskeline hiding (display)
 
+data REPL
+  = Sarith
+  | Sulamb
+  deriving (Eq, Ord, Show, Enum)
+
 main :: IO ()
-main = runInputT defaultSettings loop
+main = runInputT defaultSettings repl
   where
-  loop = getInputLine "tapl> " >>= \case
-    Nothing -> outputStrLn "Goodbye"
-    Just input -> (liftIO $ Ch4.process input) >> loop
+  repl = do
+    mode <- ask
+    loop mode
+  loop mode = do
+    expr <- getInputLine $ header mode
+    case expr of
+      Nothing -> outputStrLn "Goodbye"
+      Just input -> (proc mode input) >> loop mode
+
+header :: REPL -> String
+header = (++ "> ") . show
+
+proc :: (MonadIO m) => REPL -> String -> m ()
+proc mode = case mode of
+  Sarith -> liftIO . Ch4.process
+  Sulamb -> liftIO . Ch7.process
+
+ask :: InputT IO REPL
+ask = do
+  mapM_ outputStrLn [
+      "TaPL-hs REPL"
+    , "========================================"
+    , "1) Sarith (from Chapter 4)"
+    , "2) Sulamb (from Chapter 7)"
+    , "----------------------------------------"
+    ]
+  lang <- getInputLine "Select Language (default: 1) >> "
+  case lang of
+    Nothing -> return Sarith
+    Just  i -> return (toEnum (read i - 1))
