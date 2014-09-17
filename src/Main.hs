708397ef9aae030525ef957c2fe8c57aab360439
VoQn <voqn.tyrantist@gmail.com>
2014-09-17 19:49:58 +0900

    Fix Main REPL

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,8 @@ import Control.Monad.Trans
 import System.Console.Haskeline hiding (display)
 
 data REPL
-  = Sarith
+  = Halt
+  | Sarith
   | Sulamb
   deriving (Eq, Ord, Show, Enum)
 
@@ -16,12 +17,14 @@ main = runInputT defaultSettings repl
   where
   repl = do
     mode <- ask
-    loop mode
+    case mode of
+      Halt -> outputStrLn "Goodbye"
+      _    -> loop mode
   loop mode = do
     expr <- getInputLine $ header mode
     case expr of
       Nothing -> outputStrLn "Goodbye"
-      Just input -> (proc mode input) >> loop mode
+      Just input -> proc mode input >> loop mode
 
 header :: REPL -> String
 header = (++ "> ") . show
@@ -36,11 +39,13 @@ ask = do
   mapM_ outputStrLn [
       "TaPL-hs REPL"
     , "========================================"
-    , "1) Sarith (from Chapter 4)"
-    , "2) Sulamb (from Chapter 7)"
+    , "0) Exit"
+    , "1) Sarith (from Chapter 3-4)"
+    , "2) Sulamb (from Chapter 5-7)"
     , "----------------------------------------"
     ]
-  lang <- getInputLine "Select Language (default: 1) >> "
+  lang <- getInputLine "Select Mode (default: 2) >> "
   case lang of
-    Nothing -> return Sarith
-    Just  i -> return (toEnum (read i - 1))
+    Nothing -> return Halt
+    Just "" -> return Sulamb
+    Just  i -> return (toEnum (read i))
