b6a92eac6016fa768bbe420d84fbb55699acff51
VoQn <voqn.tyrantist@gmail.com>
2014-09-18 18:04:31 +0900

    Split out NumVal data-type from Syntax.hs

---
+++ src/Chapter8/NumVal.hs
@@ -0,0 +1,43 @@
+module Chapter8.NumVal where
+
+import Data.Monoid
+import Data.Display
+
+data NumVal
+  = NumZero
+  | NumSucc NumVal
+  deriving (Eq, Show)
+
+toRealNum :: NumVal -> Integer
+toRealNum = walk 0
+  where
+  walk c nv = case nv of
+    NumZero   -> c
+    NumSucc v -> walk (c + 1) v
+
+instance Ord NumVal where
+  compare n1 n2 = toRealNum n1 `compare` toRealNum n2
+
+instance Enum NumVal where
+  fromEnum = fromInteger . toRealNum
+
+  toEnum = walk NumZero
+    where
+    walk nv n
+      | n > 0     = walk (NumSucc nv) $ n - 1
+      | otherwise = nv
+
+  pred nv = case nv of
+    NumZero   -> NumZero
+    NumSucc v -> v
+
+instance Monoid NumVal where
+  mempty = NumZero
+  mappend nv1 nv2 = case (nv1, nv2) of
+    (NumZero, NumZero)     -> NumZero
+    (NumZero, _)           -> nv2
+    (_, NumZero)           -> nv1
+    (NumSucc v, NumSucc w) -> NumSucc $ NumSucc $ mappend v w
+
+instance Display NumVal where
+  toDisplay = toDisplay . fromEnum
