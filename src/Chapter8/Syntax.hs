53a62bd07f9751ea18dcc0c05213bd6f2a33bf3e
VoQn <voqn.tyrantist@gmail.com>
2014-09-19 02:28:10 +0900

    Added Tests about Term data-type

--- src/Chapter8/Syntax.hs
+++ src/Chapter8/Syntax.hs
@@ -34,33 +34,32 @@ instance HasType Val where
     ValFalse -> Right TyBool
     ValNum _ -> Right TyNat
 
+requireType :: Ty -> Ty -> Term -> Either TypeError Ty
+requireType req ret term = case typeof term of
+  Right ty
+    | ty == req -> Right ret
+    | otherwise -> Left $ MismatchWithRequire req ty
+  Left  err     -> Left err
+
+requireSameType :: (String, Term) -> (String, Term) -> Either TypeError Ty
+requireSameType (lt,tt) (lf,tf) = case (typeof tt, typeof tf) of
+  (Left err, _) -> Left err
+  (_, Left err) -> Left err
+  (Right t, Right f)
+    | t == f    -> Right t
+    | otherwise -> Left $ MultiTypeReturn [(lt, t), (lf, f)]
+
 instance HasType Term where
   typeof term = case term of
     TmTrue  -> Right TyBool
     TmFalse -> Right TyBool
     TmZero  -> Right TyNat
 
-    TmIsZero t -> case typeof t of
-      Right TyNat -> Right TyBool
-      Right other -> Left $ MismatchWithRequire TyNat other
-      Left  err   -> Left err
+    TmIsZero t -> requireType TyNat TyBool t
+    TmSucc   t -> requireType TyNat TyNat  t
+    TmPred   t -> requireType TyNat TyNat  t
 
-    TmSucc t -> case typeof t of
-      Right TyNat -> Right TyNat
-      Right other -> Left $ MismatchWithRequire TyNat other
-      Left  err   -> Left err
-
-    TmPred t -> case typeof t of
-      Right TyNat -> Right TyNat
-      Right other -> Left $ MismatchWithRequire TyNat other
-      Left  err   -> Left err
-
-    TmIf p t f -> case typeof p of
-      Right TyBool -> case (typeof t, typeof f) of
-        (Right t', Right f')
-          | t' == f'  -> Right t'
-          | otherwise -> Left $ MultiTypeReturn [("then", t'), ("else", f')]
-        (Left err, _) -> Left err
-        (_, Left err) -> Left err
-      Right ty  -> Left $ MismatchWithRequire TyBool ty
-      Left  err -> Left err
+    TmIf p t f -> predCheck =<< restCheck
+      where
+        restCheck   = requireSameType ("then", t) ("else", f)
+        predCheck r = requireType TyBool r p
--- test/Chapter8/SyntaxSpec.hs
+++ test/Chapter8/SyntaxSpec.hs
@@ -22,6 +22,17 @@ instance Arbitrary Term where
     gBool    = oneof [gBoolVal, gIsZero, gIfBool]
     gNum     = oneof [gNumVal,  gNumApp, gIfNum]
 
+newtype WrongTerm = MkWrongTerm { unWrong :: Term }
+  deriving (Eq, Show)
+
+beWrong :: Term -> WrongTerm
+beWrong = MkWrongTerm
+
+instance Arbitrary WrongTerm where
+  arbitrary = oneof [gWrongNumApp]
+    where
+    gWrongNumApp = beWrong . TmIsZero <$> elements [TmTrue, TmFalse]
+
 spec :: Spec
 spec = do
   describe "Term data-type" $ do
@@ -41,14 +52,25 @@ spec = do
 
     describe "as an instance of HasType type-class" $ do
 
-      it "typeof TmTrue => TyBool" $ do
+      it "typeof TmTrue => TyBool" $
         typeof TmTrue `shouldBe` Right TyBool
 
-      it "typeof TmFalse => TyBool" $ do
+      it "typeof TmFalse => TyBool" $
         typeof TmFalse `shouldBe` Right TyBool
 
-      it "typeof TmZero => TyNat" $ do
+      it "typeof TmZero => TyNat" $
         typeof TmZero `shouldBe` Right TyNat
+
+      it "typeof (TmSucc TmZero) => TyNat" $
+        typeof (TmSucc TmZero) `shouldBe` Right TyNat
+
+      it "typeof (TmPred $ TmSucc TmZero) => TyNat" $
+        typeof (TmPred $ TmSucc TmZero) `shouldBe` Right TyNat
+
+      prop "typeof (TmSucc $ [TYPE_ERROR_TERM]) => TypeError" $
+        \(t :: WrongTerm) -> do
+          typeof (TmSucc $ unWrong t) `shouldBe`
+            Left (MismatchWithRequire TyNat TyBool)
 
       it "typeof (TmIsZero TmZero) => TyBool" $
         typeof (TmIsZero TmZero) `shouldBe` Right TyBool
@@ -56,3 +78,21 @@ spec = do
       it "typeof (TmIsZero TmTrue) => TypeError" $
         typeof (TmIsZero TmTrue) `shouldBe`
           Left (MismatchWithRequire TyNat TyBool)
+
+      it "typeof (TmIf (TmIsZero TmZero) TmTrue TmFalse)" $
+        typeof (TmIf (TmIsZero TmZero) TmTrue TmFalse) `shouldBe`
+          Right TyBool
+
+      it "typeof (TmIf (TmIsZero TmZero) TmTrue TmFalse)" $
+        typeof (TmIf TmTrue (TmSucc TmZero) TmFalse) `shouldBe`
+          Left (MultiTypeReturn [("then", TyNat), ("else", TyBool)])
+
+      prop "typof (TmIf [TypeSafe] [TYPE_ERROR_TERM] [TypeSafe])" $
+        \(e :: WrongTerm) -> do
+          typeof (TmIf TmTrue (unWrong e) TmTrue) `shouldBe`
+            Left (MismatchWithRequire TyNat TyBool)
+
+      prop "typof (TmIf [TypeSafe] [TypeSafe] [TYPE_ERROR_TERM])" $
+        \(e :: WrongTerm) -> do
+          typeof (TmIf TmTrue TmTrue (unWrong e)) `shouldBe`
+            Left (MismatchWithRequire TyNat TyBool)
