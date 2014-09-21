52defa9294546985e67e08c861935258538e009c
VoQn <voqn.tyrantist@gmail.com>
2014-09-21 11:59:58 +0900

    #5 Implement eval

--- src/Chapter8/Eval.hs
+++ src/Chapter8/Eval.hs
@@ -1,13 +1,36 @@
 {-# LANGUAGE LambdaCase #-}
-module Chapter8.Eval where
+{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
+module Chapter8.Eval (eval) where
 
+import Control.Applicative
 import Chapter8.Type
 import Chapter8.NumVal
 import Chapter8.Syntax
 
 eval :: Term -> Either TypeError Val
-eval = \case
-  TmTrue  -> Right ValTrue
-  TmFalse -> Right ValFalse
-  TmZero  -> Right $ ValNum NumZero
-  _ -> undefined
+eval term = typeof term >> eval' term
+
+eval' :: Term -> Either TypeError Val
+eval' = \case
+  TmTrue  -> return $ ValTrue
+  TmFalse -> return $ ValFalse
+  TmZero  -> return $ ValNum NumZero
+
+  TmSucc   t -> vSucc <$> eval' t
+  TmPred   t -> vPred <$> eval' t
+  TmIsZero t -> vIsZero <$> eval' t
+
+  TmIf p t f -> eval' p >>= \case
+    ValTrue  -> eval' t
+    ValFalse -> eval' f
+
+vSucc :: Val -> Val
+vSucc (ValNum nv) = ValNum $ NumSucc nv
+
+vPred :: Val -> Val
+vPred (ValNum (NumSucc nv)) = ValNum nv
+vPred v = v
+
+vIsZero :: Val -> Val
+vIsZero (ValNum NumZero) = ValTrue
+vIsZero _ = ValFalse
--- test/Chapter8/EvalSpec.hs
+++ test/Chapter8/EvalSpec.hs
@@ -17,3 +17,41 @@ spec = do
 
     it "eval (0 :: Term) => (0 :: Val)" $
       eval TmZero `shouldBe` Right (ValNum NumZero)
+
+    it "eval (succ 0) => (1 :: Val)" $
+      eval (TmSucc TmZero) `shouldBe` Right (ValNum $ NumSucc NumZero)
+
+    it "eval (pred 0 :: Term) => (0 :: Val)" $
+      eval (TmPred TmZero) `shouldBe` Right (ValNum $ NumZero)
+
+    it "eval (pred succ pred succ 0) => (0 :: Val)" $ do
+      let term = TmPred . TmSucc . TmPred . TmSucc $ TmZero
+      eval term `shouldBe` Right (ValNum NumZero)
+
+    it "eval (pred pred pred succ succ succ 0) => (0 :: Val)" $ do
+      let term = TmPred . TmPred . TmPred . TmSucc . TmSucc . TmSucc $ TmZero
+      eval term `shouldBe` Right (ValNum NumZero)
+
+    it "eval (zero? 0 :: Term) => (true :: Val)" $
+      eval (TmIsZero TmZero) `shouldBe` Right ValTrue
+
+    it "eval (zero? 1 :: Term) => (false :: Val)" $
+      eval (TmIsZero $ TmSucc TmZero) `shouldBe` Right ValFalse
+
+    it "eval (zero? (pred succ pred 0)) => (true :: Val)" $ do
+      let term = TmIsZero $ TmPred $ TmSucc $ TmPred $ TmSucc TmZero
+      eval term `shouldBe` Right ValTrue
+
+    it "eval (if (zero? 0) true false) => (false :: Val)" $ do
+      let term = TmIf (TmIsZero TmZero) TmTrue TmFalse
+      eval term `shouldBe` Right ValTrue
+
+  describe "comlex terms" $
+    it "eval (succ (if false 0 (succ 0))) => (succ succ 0)" $ do
+      let term = TmSucc $ TmIf TmFalse TmZero $ TmSucc TmZero
+      eval term `shouldBe` Right (ValNum . NumSucc . NumSucc $ NumZero)
+
+  describe "error" $ do
+
+    it "eval (zero? true)" $ do
+      eval (TmIsZero TmTrue) `shouldBe` Left (MismatchWithRequire TyNat TyBool)
