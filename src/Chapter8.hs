e4c4577ea1aad9bf2c2b5e2dc20a2954e38ea5e5
VoQn <voqn.tyrantist@gmail.com>
2014-09-23 03:51:26 +0900

    #6 implement interpreter process

--- src/Chapter8.hs
+++ src/Chapter8.hs
@@ -11,7 +11,12 @@ import Data.Display
 import Chapter8.Type
 import Chapter8.NumVal
 import Chapter8.Syntax
+import Chapter8.Parser
 import Chapter8.Eval
 
 process :: String -> IO ()
-process = undefined
+process line = case parseTerm line of
+  Left  err  -> print err
+  Right term -> case eval term of
+    Left err    -> display err
+    Right value -> display value
