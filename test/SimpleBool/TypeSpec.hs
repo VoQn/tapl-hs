2443264e581d5d485ae350fce49fa810dca95f87
VoQn <voqn.tyrantist@gmail.com>
2014-10-02 21:54:23 +0900

    Added tests for SimpleBool Type data-type as an instance of Display type-class

--- test/SimpleBool/TypeSpec.hs
+++ test/SimpleBool/TypeSpec.hs
@@ -9,7 +9,7 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
--- import Data.Display
+import Data.Display
 import SimpleBool.Type
 
 instance Arbitrary Type where
@@ -33,3 +33,11 @@ spec = do
 
       prop "show" $ \(x :: Type) ->
         showList [x] `seq` showsPrec 0 x `seq` show x `seq` True
+
+    describe "as an instance of Display type-class" $ do
+
+      it "display TyBool => \"Bool\"" $
+        toDisplay TyBool `shouldBe` "Bool"
+
+      it "display (TyArr TyBool TyBool) => \"Bool -> Bool\"" $
+        toDisplay (TyArr TyBool TyBool) `shouldBe` "Bool -> Bool"
